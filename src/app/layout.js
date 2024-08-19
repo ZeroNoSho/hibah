b7cbd856c46ef56fc4f9032ec0337867bad8c7c9
ZeroNoSho <inukuro351@gmail.com>
2024-08-20 00:44:45 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/app/layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </head>
       <body className={inter.className}>
         <Provider>{children}</Provider>
       </body>
